0a41bc91041d7915252b4bed74c773d3801b0465
juancamilo <jcamil-2010@hotmail.com>
2021-10-25 13:28:36 -0500

    Retos modificados para evitar copia

---
+++ src/main/java/com/Retos/CicloTres/repository/crud/CabinCrudRepository.java
@@ -0,0 +1,8 @@
+package com.Retos.CicloTres.repository.crud;
+
+import com.Retos.CicloTres.model.Cabin;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CabinCrudRepository extends CrudRepository<Cabin, Integer>{
+    
+}
